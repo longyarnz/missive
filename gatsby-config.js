002a37b97ab40c90c0471fde8deb23cf5b5d327a
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-11 09:32:35 +0100

    Configure css options to  remove unused css and reduce file size

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,23 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [
-          require("tailwindcss"),
-          // require("./tailwind.config.js"),  Optional: Load custom Tailwind CSS configuration
+          require(`tailwindcss`),
+          require(`autoprefixer`),
+          require(`./tailwind.config.js`), // Optional: Load custom Tailwind CSS configuration
         ],
-      }
+      },
     },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  corePlugins: {
+    float: false
+  },
+  theme: {
+    fontFamily: {
+      sans: ['DM Sans', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans'],
+    },
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      screens: {
+        md: '768px',
+        lg: '1024px',
+      },
+      gray: {
+        100: '#f9f9fb',
+        150: '#f0f0fa',
+        200: '#e9e9f7',
+        300: '#dcddf4',
+        400: '#c8c9e4',
+        500: '#acacd2',
+        600: '#7c7fb6',
+        700: '#5e608d',
+        800: '#3f405f',
+        900: '#171721',
+      },
+      text: {
+        300: '#d4d5ed',
+        400: '#9496cc',
+        500: '#6366ab',
+        600: '#33307e',
+        700: '#0a0d4d',
+      },
+      hightlight: '#383dd3',
+      red: '#e66181',
+      orange: '#fc8181',
+      green: '#6ec48f',
+      extend: {}
+    },
+    variants: {},
+    plugins: []
+  }
+}
