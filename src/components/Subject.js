9b0bb266925ffd6485d8bf593c87174022262f52
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-12 23:36:36 +0100

    Complete subject view without search bar

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import SearchBar from './SearchBar'
+import styles from '../styles/subject.module.css'
+import welcome from '../images/welcome.png'
+
+const className = styles['subject'];
 
 export default function Subject() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className={className}>
+            <div>
+                <SearchBar />
+                <img src={welcome} alt="welcome"/>
+                <h2>Welcome to your mailx dashboard.</h2>
+                <p>Connect an account to start managing your email.</p>
+                <button>Connect an account</button>
+            </div>
+        </section>
+    )
 }
