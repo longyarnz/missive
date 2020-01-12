c6eae790a12e0ff0da281277df4c1fc9a6127ec4
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-12 16:13:20 +0100

    Compose dashboard with components

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Subject from '../components/Subject'
+import View from '../components/View'
+import styles from '../styles/dashboard.module.css'
 
 export default function Dashboard() {
+    const className = `${styles['section']}`;
     return (
-        <section>
-            
+        <section className={className}>
+            <Sidebar />
+
+            <Subject />
+
+            <View />
         </section>
     )
 }
