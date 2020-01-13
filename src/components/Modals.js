c122ca00a84a52c0d1da99ce480801cf35ad17b9
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-13 13:44:28 +0100

    Restructure team set up

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ShouldRender from '@bit/lekanmedia.shared-ui.internal.should-render'
-import CreateTeamModal from './CreateTeamModal'
+import { CreateTeamModal, InviteMembersModal } from './TeamModal'
 import styles from '../styles/modals.module.css'
 
 const className = styles['modals'];
@@ -8,6 +8,7 @@ const className = styles['modals'];
 export default function Modals(props) {
     const { pathname } = props.location;
     const createTeam = pathname.includes('/team/create');
+    const inviteMembers = pathname.includes('/team/invite');
 
     if (/dashboard(\/*)$/.test(pathname)) return null;
 
@@ -15,6 +16,10 @@ export default function Modals(props) {
         <section className={className}>
             <ShouldRender if={createTeam}>
                 <CreateTeamModal />
+            </ShouldRender> 
+
+            <ShouldRender if={inviteMembers}>
+                <InviteMembersModal />
             </ShouldRender> 
         </section>
     )
