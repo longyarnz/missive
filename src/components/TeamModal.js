25f6743811103f441ee3579d21dc1847b6e831d4
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-13 19:58:43 +0100

    Complete team invite modal - Add scroll nice feature - Add state to team invite modal - Render input elements from a dynamic list

--- src/components/TeamModal.js
+++ src/components/TeamModal.js
@@ -1,29 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { navigate } from 'gatsby'
 import { Helmet } from 'react-helmet'
-import styles from '../styles/create-team.module.css'
+import { FlatList } from '@bit/lekanmedia.shared-ui.internal.utils'
 import { CloseModalButton } from './IconButton';
 import { PrimaryButton } from './Button';
+import styles from '../styles/create-team.module.css'
 import teamBanner from '../images/team-banner.png'
 
+const emails = {};
 const className = styles['createTeam'];
+const goBack = () => navigate('/dashboard/');
+const goToInvite = () => navigate('/dashboard/team/invite');
+const addMails = ({ target }) => emails[target.name] = target.value;
 
-export function CreateTeamModal(props) {
-    const goBack = () => navigate('/dashboard');
-    const goToInvite = () => navigate('/dashboard/team/invite');
-
+export function CreateTeamModal() {
+    console.log(styles['createTeam']);
     return (
         <div className={className}>
             <Helmet>
                 <title>Setup your team</title>
             </Helmet>
-            
+
             <div className={styles['closeWrapper']}>
                 <CloseModalButton onClick={goBack} />
             </div>
 
             <div className={styles['imgWrapper']}>
-                <img src={teamBanner} alt="team banner"/>
+                <img src={teamBanner} alt="team banner" />
             </div>
 
             <h4>
@@ -39,11 +42,12 @@ export function CreateTeamModal(props) {
     )
 }
 
-export function InviteMembersModal(props) {
-    const goBack = () => navigate('/dashboard/');
+export function InviteMembersModal() {
+    const [ people, setPeople ] = useState(1);
+    const list = new Array(people).fill('');
 
     return (
-        <div className={className}>
+        <div className={`${className} ${styles['invite']}`}>
             <Helmet>
                 <title>Invite Team Members</title>
             </Helmet>
@@ -53,11 +57,11 @@ export function InviteMembersModal(props) {
             </div>
 
             <div className={styles['imgWrapper']}>
-                <img src={teamBanner} alt="team banner"/>
+                <img src={teamBanner} alt="team banner" />
             </div>
 
             <h4>
-                Invite team members via their emails.
+                Invite team members via <br /> their emails.
             </h4>
 
             <p>
@@ -65,10 +69,27 @@ export function InviteMembersModal(props) {
             </p>
 
             <div className={styles['inputWrapper']}>
-                <input type="text" name="team" spellCheck="false" />
+                <FlatList
+                    list={list}
+                    listView={(i, o) => (
+                        <input 
+                            key={`input-${o}`} 
+                            type="email" 
+                            name={`team${o}`} 
+                            spellCheck="false" 
+                            onChange={addMails}
+                        />
+                    )}
+                />
             </div>
 
-            <PrimaryButton text="Add 1 person" />
+            <p>
+                <button onClick={() => setPeople(people + 1)}>
+                    Add more people.
+                </button>
+            </p>
+
+            <PrimaryButton text="Invite People" />
         </div>
     )
 }
