0b5c654024ae443ae9296c20bca64fef29213e63
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-14 08:28:10 +0100

    Add disabled function to primary and secondary button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,17 +4,17 @@ import styles from '../styles/button.module.css'
 const primary = styles['primary'];
 const secondary = styles['secondary'];
 
-export function PrimaryButton({ text, onClick }) {
+export function PrimaryButton({ text, onClick, disabled }) {
     return (
-        <button className={primary} onClick={onClick}>
+        <button className={primary} onClick={onClick} disabled={disabled}>
             {text}
         </button>
     )
 }
 
-export function SecondaryButton({ text, onClick }) {
+export function SecondaryButton({ text, onClick, disabled }) {
     return (
-        <button className={secondary} onClick={onClick}>
+        <button className={secondary} onClick={onClick} disabled={disabled}>
             {text}
         </button>
     )
