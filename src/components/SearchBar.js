0c16adacefc05cff71e635e74e6ed93053699d53
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-14 09:53:18 +0100

    Create team search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import styles from '../styles/search-bar.module.css'
 
-const className = `${styles['searchBar']}`;
+const search = `${styles['search']}`;
+const team = `${styles['team']}`;
 
 export default function SearchBar() {
     return (
-        <div className={className}>
+        <div className={search}>
             <i className="material-icons">search</i>
             <input type="text" name="search" placeholder="Search" />
+        </div>
+    )
+}
+
+export function TeamSearchBar() {
+    return (
+        <div className={team}>
+            <i className="material-icons">search</i>
+            <input type="text" name="search" placeholder="Search for specific team members" />
         </div>
     )
 }
