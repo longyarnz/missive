dfe14ae97be91f1ae85856b569fea31227e68e8b
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-16 12:27:37 +0100

    Send reset link to email

--- src/pages/reset.js
+++ src/pages/reset.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import styles from '../styles/form.module.css'
 import caption from '../images/forgot.png'
 
 export default function ForgotPassword() {
+    const [success, setSuccess] = useState(null);
     const className = `${styles['section']} container mx-auto`;
+
+    const resetPassword = e => {
+        e.preventDefault();
+        setSuccess('Reset link has been sent to your email');
+    }
+
     return (
         <section className={className}>
             <div>
@@ -14,9 +21,10 @@ export default function ForgotPassword() {
             <div>
                 <h1>Let's get you back into <br /> your account.</h1>
                 <h2>Enter the email you registered your <br /> account with.</h2>
-                <form method="post">
-                    <input type="email" name="email" placeholder="Email Address" />
-                    <input className={styles['reset']} type="button" value="Send reset password link"/>
+                <form method="post" onSubmit={resetPassword}>
+                    <p id={styles['success']}>{success}</p>
+                    <input type="email" name="email" placeholder="Email Address" required />
+                    <input className={styles['reset']} type="submit" value="Send reset password link"/>
                     <footer className="text-center">
                         <Link to="/login">Back to log in</Link>
                     </footer>
