a8ae3e24f4b7a8d8cd97738d5526344c09278ce1
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-16 10:01:53 +0100

    Create team menu

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../styles/user-menu.module.css'
 import placeholder from '../images/avatar.png'
 
-export default function UserMenu({ src, name }) {
+export function UserMenu({ src, name }) {
     const className = styles['userMenu'];
     src = src || placeholder;
     return (
@@ -11,5 +11,17 @@ export default function UserMenu({ src, name }) {
             <span>{name}</span>
             <i className="material-icons">more_vert</i>
         </footer>
+    )
+}
+
+export function TeamMenu({ src, name }) {
+    const className = `${styles['userMenu']} ${styles['teamMenu']}`;
+    src = src || placeholder;
+    return (
+        <p className={className}>
+            <img src={src} alt="avatar"/>
+            <span>{name}</span>
+            <i className="material-icons">more_horiz</i>
+        </p>
     )
 }
