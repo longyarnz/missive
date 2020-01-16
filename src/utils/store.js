f2f5c9b1c57bfdc7e5d13ee0ce589479356607ce
Ayodele Olalekan <longyarnz@gmail.com>
2020-01-16 12:27:12 +0100

    Add login method to store

--- src/utils/store.js
+++ src/utils/store.js
@@ -20,9 +20,15 @@ export default class Store {
         db.filter(e => e.email !== email);
         localStorage.MISSIVE_USERS = JSON.stringify(db);
     }
-
+    
     static checkUser(email) {
         const db = localStorage.MISSIVE_USERS;
         return db ? db.includes(email) : false;
+    }
+    
+    static logUser(email, password) {
+        let db = localStorage.MISSIVE_USERS;
+        db = db ? JSON.parse(db) : [];
+        return db.some(e => e.email === email && e.password === password);
     }
 }
